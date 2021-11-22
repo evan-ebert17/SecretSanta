13e53c2911e6505390f48ebf4e595d9aad9306d1
Dave Strider <evan.ebert17@gmail.com>
2021-11-21 23:27:30 -0600

    added appending li's, pushing into an array the names you put into the text field, and css touchups

--- assets/santa.js
+++ assets/santa.js
@@ -1,27 +1,26 @@
 var nameEnter = document.getElementById("nameEnter");
-// var txtField = document.querySelector('#hello');
-
 var nameAdd = document.getElementById('nameAdd');
 var nameSubmit = document.getElementById('nameSubmit');
 var nameList = document.getElementById('namesList');
-
-var inputText = nameEnter.nodeValue
 
 var nameArray = [];
 
 nameAdd.addEventListener('click', function (event) {
     event.preventDefault();
-    console.log(inputText);
-    // nameArray.push(inputText);
-    // console.log(nameArray);
-    // if (inputText.value == '') {
-    //     return
-    // } else {
-    //     var newName = document.createElement('li');
-    //     newName.textContent = inputText;
-    // }
+    let inputText = nameEnter.value;
+    if (inputText === '') {
+        return
+    } else {
+        nameArray.push(inputText);
+        var newName = document.createElement('li');
+        newName.textContent = inputText;
+        nameList.appendChild(newName);
+        console.log(nameArray)
+        nameEnter.value = '';
+    }
+    
 })
 
 nameSubmit.addEventListener('click', function () {
-    console.log('hello' + `${inputText}`);
+    
 })
