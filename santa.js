ccde43dd6b1c68ea6872eb4ebec40102a2ffbb3a
Dave Strider <evan.ebert17@gmail.com>
2021-11-21 19:59:06 -0600

    added style sheet and css variables

--- santa.js
+++ santa.js
@@ -1,1 +1,13 @@
-var nameButton = document.getElementById('')
+var nameInput = document.getElementById('nameEnter');
+var nameButton = document.getElementById('nameSubmit');
+var nameList = document.getElementById('namesList');
+
+var inputText = nameInput;
+
+
+
+var nameArray = [];
+
+nameButton.addEventListener('click', function() {
+    console.log('hello' + `${inputText}`);
+})
